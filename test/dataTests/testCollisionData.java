3646b84b669413722f68f530725eeb893e8cef1e
calebsanfo <calebsanfo@gmail.com>
2020-04-16 11:11:25 -0400

    added test files

---
+++ test/dataTests/testCollisionData.java
@@ -0,0 +1,10 @@
+package dataTests;
+
+import org.junit.jupiter.api.Test;
+
+public class testCollisionData {
+
+  @Test
+  void name() {
+  }
+}
---
+++ test/dataTests/testLevelData.java
@@ -0,0 +1,15 @@
+package dataTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ooga.data.Collision;
+import org.junit.jupiter.api.Test;
+
+class testLevelData {
+
+  @Test
+  void textXML() {
+    Collision collision = new Collision("collisionsTest.xml");
+
+  }
+}
